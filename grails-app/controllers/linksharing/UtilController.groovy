79f08671f9ba439a6670ca2aa6f49be98649fb3c
anuj.gupta <anuj.gupta@tothenew.com>
2018-10-16 16:43:26 +0530

    Initial Commit of LinkSharing(Main file)

---
+++ grails-app/controllers/linksharing/UtilController.groovy
@@ -0,0 +1,22 @@
+package linksharing
+
+
+import groovy.util.logging.Slf4j
+
+@Slf4j
+class UtilController {
+
+    def index() {
+        log.trace("Trace Message!");
+        log.debug("Debug Message!");
+        log.info("Indexing")
+        log.info(params.toQueryString())
+        render "asdjkashjdhskja"
+    }
+
+    def show(){
+        log.info("ShowMethod")
+        log.trace("Trace Message!");
+        log.debug("Debug Message!");
+    }
+}
